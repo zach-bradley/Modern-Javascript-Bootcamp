f0d1ccfab6ab1a5cc02b266011e9bc6ad3190d52
Zach Bradley <zachbradleydev@gmail.com>
2019-12-02 17:30:03 -0500

    seperated debounce funtion to seperate file, added movie menu

--- Projects/Movie-Fights/index.js
+++ Projects/Movie-Fights/index.js
@@ -6,24 +6,25 @@ const fetchData = async (searchTerm) => {
     }
   });
 
-  console.log(response.data);
+  if (response.data.Error) {
+    return [];
+  }
+  return response.data.Search;
 };
 
 const input = document.querySelector('input');
 
-const debounce = (func, delay = 1000) => {
-  let timeoutId;
-  return (...args) => {
-    if(timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      func.apply(null, args);
-    }, delay);
-  };
-}; 
+const onInput = async event => {
+  const movies = await fetchData(event.target.value);
+  
+  for (let movie of movies) {
+    const div = document.createElement('div');
+    div.innerHTML = `
+      <img src="${movie.Poster}" />
+      <h1>${movie.Title}</h1>
+    `;
 
-const onInput = event => {
-  fetchData(event.target.value);
+    document.querySelector('#target').appendChild(div);
+  }
 };
 input.addEventListener('input', debounce(onInput, 500));
