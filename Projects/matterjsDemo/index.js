6efccea10fa3be930d892c6497d95f6904bffde7
Zach Bradley <zachbradleydev@gmail.com>
2019-12-03 18:44:42 -0500

    added borders to demo and shape with ability to move

--- Projects/matterjsDemo/index.js
+++ Projects/matterjsDemo/index.js
@@ -1,5 +1,5 @@
 //Boiler-plate for matter.js
-const { Engine, Render, Runner, World, Bodies} = Matter;
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse} = Matter;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -15,9 +15,25 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-//Create object 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-  isStatic: true
-});
-//Add object to world
-World.add(world, shape);
+World.add(world, MouseConstraint.create(engine, {
+  mouse: Mouse.create(render.canvas)
+}));
+
+// //Create object 
+// const shape = Bodies.rectangle(200, 200, 50, 50, {
+//   isStatic: true
+// });
+// //Add object to world
+// World.add(world, shape);
+
+//Walls
+const walls = [
+  Bodies.rectangle(400, 0, 800, 20, {isStatic: true}),
+  Bodies.rectangle(400, 600, 800, 20, {isStatic: true}),
+  Bodies.rectangle(0, 300, 20, 600, {isStatic: true}),
+  Bodies.rectangle(800, 300, 20, 600, {isStatic: true}),
+];
+
+World.add(world, walls);
+
+World.add(world, Bodies.rectangle(200, 200, 50, 50));
