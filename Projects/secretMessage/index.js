4a0e9195fa1473a2c6b53ba9162f0a2c7f7d4226
Zach Bradley <zachbradleydev@gmail.com>
2019-12-05 17:15:26 -0500

    build second form to show link and ability to visit link to see message

--- Projects/secretMessage/index.js
+++ Projects/secretMessage/index.js
@@ -1,9 +1,32 @@
+const { hash } = window.location;
+
+const message = atob(hash.replace('#', ''));
+
+if (message) {
+  document.querySelector("#message-form").classList.add('hide');
+  document.querySelector("#message-show").classList.remove('hide');
+
+  document.querySelector('h1').innerHTML = message;
+}
+
+
+
 document.querySelector("form").addEventListener('submit', event => {
   event.preventDefault();
+
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#link-form').classList.remove('hide');
 
   const input = document.querySelector('#message-input');
   const encrypted = btoa(input.value);
 
-  document.querySelector('#link-input').value = encrypted
-  
+  const linkInput = document.querySelector('#link-input');
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
+
 });
+
+const copy = () => {
+  document.execCommand("copy");
+  alert("Copied text!");
+}
