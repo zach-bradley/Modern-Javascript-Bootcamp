a3a492fdeeaa46d7aa4fa196e37ea98ff81b4690
Zach Bradley <zachbradleydev@gmail.com>
2019-12-11 17:37:57 -0500

    finished authentication, pre cleanup

--- Projects/e-commerce-app/repositories/users.js
+++ Projects/e-commerce-app/repositories/users.js
@@ -43,6 +43,21 @@ class UsersRepository {
     return record;
   }
 
+  async comparePasswords(saved, supplied) {
+    //Saved -> password saved in out database. "hashed.salt"
+    //Supplied -> password given to us by user trying to sign in
+
+    //Without destructuring
+    // const result = saved.split('.');
+    // const hashed = result[0];
+    // const salt = result[1];
+
+    const [hashed, salt] = saved.split('.');
+    const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
+
+    return hashed === hashedSuppliedBuf.toString('hex');
+  }
+
   async writeAll(records) {
     await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
   }
