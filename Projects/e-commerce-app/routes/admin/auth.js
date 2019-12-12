e58e421d9e6d9b6aa54dc98d3d1772033767f18a
Zach Bradley <zachbradleydev@gmail.com>
2019-12-12 00:11:32 -0500

    pulled validators for sign in into a seperate file

--- Projects/e-commerce-app/routes/admin/auth.js
+++ Projects/e-commerce-app/routes/admin/auth.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
+const { requireEmail, requirePassword, requirePasswordConfirmation } = require('./validators');
 
 const router = express.Router();
 
@@ -9,24 +12,23 @@ router.get('/signup', (req, res) => {
   res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
+router.post(
+  '/signup', 
+  [requireEmail, requirePassword, requirePasswordConfirmation], 
+  async (req, res) => {
+    const errors = validationResult(req);
+    
+    if(!errors.isEmpty()) {
+      return res.send(signupTemplate({ req, errors }));
+    }
 
-  const existingUser = await usersRepo.getOneBy({ email});
+    const { email, password, passwordConfirmation } = req.body;
+    const user = await usersRepo.create({email, password});
 
-  if (existingUser) {
-    return res.send("That user already exists");
-  }
-  if (password !== passwordConfirmation) {
-    return res.send("Passwords must match");
-  }
-  //Create a user in our user repo to represent this person
-  const user = await usersRepo.create({email, password})
 
-  //Store id of that user inside the users cookie
-  req.session.userId = user.id
+    req.session.userId = user.id
 
-  res.send('Account created!')
+    res.send('Account created!')
 });
 
 router.get('/signout', (req, res) => {
@@ -38,7 +40,9 @@ router.get('/signin', (req, res) => {
   res.send(signinTemplate());
 });
 
-router.post('/signin', async (req, res) => {
+router.post('/signin', [
+  
+], async (req, res) => {
   const { email, password} = req.body;
 
   const user = await usersRepo.getOneBy({ email });
