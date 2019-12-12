6ecc31ca3f0278d9b47d4f6d1dcd93d2fc0c628e
Zach Bradley <zachbradleydev@gmail.com>
2019-12-12 10:56:29 -0500

    finished validators for sign in and added helpers file for get error function

--- Projects/e-commerce-app/routes/admin/auth.js
+++ Projects/e-commerce-app/routes/admin/auth.js
@@ -4,7 +4,13 @@ const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
-const { requireEmail, requirePassword, requirePasswordConfirmation } = require('./validators');
+const { 
+  requireEmail, 
+  requirePassword, 
+  requirePasswordConfirmation, 
+  requireEmailExists, 
+  requireValidPasswordForUser 
+} = require('./validators');
 
 const router = express.Router();
 
@@ -25,7 +31,6 @@ router.post(
     const { email, password, passwordConfirmation } = req.body;
     const user = await usersRepo.create({email, password});
 
-
     req.session.userId = user.id
 
     res.send('Account created!')
@@ -37,32 +42,24 @@ router.get('/signout', (req, res) => {
 });
 
 router.get('/signin', (req, res) => {
-  res.send(signinTemplate());
+  res.send(signinTemplate({}));
 });
 
 router.post('/signin', [
-  
-], async (req, res) => {
-  const { email, password} = req.body;
+  requireEmailExists, requireValidPasswordForUser
+  ], 
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.send(signinTemplate({ errors }))
+    }
+    const { email  } = req.body;
 
-  const user = await usersRepo.getOneBy({ email });
+    const user = await usersRepo.getOneBy({ email });
 
-  if (!user) {
-    return res.send('Email not found');
-  }
+    req.session.userId = user.id;
 
-  const validPassword = await usersRepo.comparePasswords(
-    user.password,
-    password
-    )
-
-  if (!validPassword) {
-    return res.send('Invalid password');
-  }
-
-  req.session.userId = user.id;
-
-  res.send('You are signed in!');
+    res.send('You are signed in!');
 });
 
 
--- Projects/e-commerce-app/views/admin/auth/signin.js
+++ Projects/e-commerce-app/views/admin/auth/signin.js
@@ -1,14 +1,18 @@
-const layout = require('../layout')
+const layout = require('../layout');
+const { getError } = require('../../helpers');
 
-module.exports = () => {
-  return layout({content: `
-    <div>
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <button>Sign In</button>
-      </form>
-    </div>
-  `
+module.exports = ({ errors }) => {
+  return layout({
+    content: `
+      <div>
+        <form method="POST">
+          <input name="email" placeholder="email" />
+          ${getError(errors, 'email')}
+          <input name="password" placeholder="password" />
+          ${getError(errors, 'password')}
+          <button>Sign In</button>
+        </form>
+      </div>
+    `
   }); 
 };
