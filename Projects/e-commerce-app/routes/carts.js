ae6b1a9ee40fc3a918faa9fa354cdc95da3afad1
Zach Bradley <zachbradleydev@gmail.com>
2019-12-19 15:53:40 -0500

    added delete item to cart

--- Projects/e-commerce-app/routes/carts.js
+++ Projects/e-commerce-app/routes/carts.js
@@ -54,7 +54,14 @@ router.get('/cart', async (req, res) => {
 
 //Receive a post request to delete an item from the cart
 router.post('/cart/products/delete', async (req, res) => {
-  
+  const { itemId } = req.body;
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  const items = cart.items.filter(item => item.id !== itemId);
+
+  await cartsRepo.update(req.session.cartId, { items });
+
+  res.redirect('/cart');
 });
 
 module.exports = router;
