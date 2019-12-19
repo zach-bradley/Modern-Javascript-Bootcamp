7f2d80c547e3ec24556762d28e51332b27ac3da0
Zach Bradley <zachbradleydev@gmail.com>
2019-12-19 12:30:49 -0500

    added products index template to show all products

--- Projects/e-commerce-app/index.js
+++ Projects/e-commerce-app/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
-const productsRouter = require('./routes/admin/products');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(
 );
 
 app.use(authRouter);
+app.use(adminProductsRouter);
 app.use(productsRouter);
 
 app.listen(3000, () => {
