145964a9f4d819e2c6cb85ec924fa432557bac12
Zach Bradley <zachbradleydev@gmail.com>
2019-12-19 14:53:51 -0500

    added carts repository and carts router

---
+++ Projects/e-commerce-app/routes/carts.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+//Receive a post request and ad an item to a cart
+router.post('/car/products', (req, res) => {
+  
+});
+
+//Receive a get request to show all items in cart 
+
+//Receive a post request to delete an item from the cart
+
+module.exports = router;
