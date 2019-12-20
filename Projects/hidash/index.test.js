ec60ccda1a65f1191bb8581841d29f5f43537ef0
Zach Bradley <zachbradleydev@gmail.com>
2019-12-19 21:25:01 -0500

    replaced test statements with strictEqual from assert library

--- Projects/hidash/index.test.js
+++ Projects/hidash/index.test.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const { forEach, map } = require('./index');
 
 const test = (desc, fn) => {
@@ -15,9 +16,7 @@ test('The forEach function', () => {
     sum += value
   });
 
-  if (sum !== 6) {
-    throw new Error('Expected summing array to equal 6');
-  }
+  assert.strictEqual(sum, 6);
 });
 
 test('The map function', () => {
@@ -25,15 +24,7 @@ test('The map function', () => {
     return value * 2
   });
 
-  if (result[0] !== 2) {
-    throw new Error(`Expected to find 2, but found ${result[0]}`)
-  }
-  if (result[1] !== 4) {
-    throw new Error(`Expected to find 2, but found ${result[1]}`)
-  }
-  if (result[2] !== 6) {
-    throw new Error(`Expected to find 2, but found ${result[2]}`)
-  }
+  assert.deepStrictEqual(result, [2, 4, 6])
 });
 
 
